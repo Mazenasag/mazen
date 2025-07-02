e3113ab57f23fd334be710e2ddf9a411fc4f2ac2
Mazen  Asag <97623453+Mazenasag@users.noreply.github.com>
2025-07-02 14:06:25 +0600

    Add files via upload

---
+++ form/contact.php
@@ -0,0 +1,27 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = strip_tags(trim($_POST["name"]));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $subject = strip_tags(trim($_POST["subject"]));
+    $message = trim($_POST["message"]);
+
+    // Set your email address here
+    $recipient = "mezonabhy@gmail.com";
+    
+    // Email content
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n";
+    $email_content .= "Subject: $subject\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Email headers
+    $headers = "From: $name <$email>";
+
+    // Send the email
+    if (mail($recipient, $subject, $email_content, $headers)) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+}
+?>
